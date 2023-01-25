88b12db79739bf4db3897819b8fbeb8cfa11c3ca
IsNotSally <verena0915@gmail.com>
2023-01-25 12:44:03 +0100

    jest setup

--- breezy-app/__tests__/pages/homepage.test.tsx
+++ breezy-app/__tests__/pages/homepage.test.tsx
@@ -1,12 +1,15 @@
 import HomePage from '@/pages/page';
 import { render, screen } from '@testing-library/react';
+import React from 'react';
+import '@testing-library/jest-dom';
+
 
 describe('HomePage', () => {
   it('should renders the landing page', () => {
     render(<HomePage />);
 
-    const heading = screen.getByText(/welcome to next\.js!/i);
+    const heading = screen.getByText('Freelancer login');
 
-    expect(heading).toBeInTheDocument();
+    expect(heading).toBeInTheDocument()
   });
 });
